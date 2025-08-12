b7a40084b227189be286ef0f169115dcbee8744b
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-08-12 18:49:39 +0200

    Update the main to test and to run the project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello via Bun!');
+import { decodeTorrent } from './bencode';
+import { TorrentMetadata } from './models/metadata';
+
+async function main(path: string) {
+  const buf: ArrayBuffer = await Bun.file(path).arrayBuffer();
+  const data: Buffer = Buffer.from(buf);
+  const torrent = decodeTorrent(data);
+  const _metadata = new TorrentMetadata(torrent);
+  process.exit(0);
+}
+
+main('./torrents/BigBuckBunny_124_archive.torrent').catch(console.error);
